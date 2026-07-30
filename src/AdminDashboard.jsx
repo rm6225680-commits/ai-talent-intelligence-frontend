edb64c9c0b933a59f3d97191647354ea3bc9c284
Ashu <rm6225680@gmail.com>
2026-07-30 15:03:46 +0530

    Create AdminDashboard component file

---
+++ src/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AdminDashboard() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Admin Dashboard</h1>
+      <p>Welcome, Admin!</p>
+    </div>
+  );
+}
+
+export default AdminDashboard;
